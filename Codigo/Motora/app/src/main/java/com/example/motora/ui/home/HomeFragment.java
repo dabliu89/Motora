ddbeb5887f75165fdc85df86c9d7526a7057b471
Maria-Eduarda-080902 <eduardasv@alu.ufc.br>
2023-10-05 01:59:15 -0300

    Entrega 01

--- Codigo/Motora/app/src/main/java/com/example/motora/ui/home/HomeFragment.java
+++ Codigo/Motora/app/src/main/java/com/example/motora/ui/home/HomeFragment.java
@@ -17,11 +17,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-<<<<<<< HEAD
 import androidx.annotation.Nullable;
-=======
 import androidx.appcompat.app.AppCompatActivity;
->>>>>>> main
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
